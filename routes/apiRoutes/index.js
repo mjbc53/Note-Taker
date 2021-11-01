12260470a82b892c63d34badfc04cbd6dcddb47e
Mark <mjbc53@gmail.com>
2021-10-31 19:37:20 -0500

    delete request/response completed. all routes are now complete just need to refactor, create test, and deploy

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,44 @@
 const router = require('express').Router()
+//get functions from lib
 const { createNewNote, validateNote }= require('../../lib/notes')
-const { notes } = require('../../data/db.json')
+//get dataset
+let { notes } = require('../../data/db.json')
+const fs = require('fs')
+const path = require('path')
 
+//get route to get all notes from dataset
 router.get('/notes', (req, res) => {
   res.json(notes)
  })
  
+// post route for added notes to the dataset
 router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString()
+
   if(!validateNote(req.body)){
     res.status(400).send('The note is not entered correctly')
   }else{
     const note = createNewNote(req.body, notes)
     res.json(note)
+  }
+})
+
+//delete route for deleting a note
+router.delete('/notes/:id', (req, res) => {
+  const { id } = req.params
+
+  const deleted = notes.find( note => note.id === id )
+
+  if(deleted){
+    notes = notes.filter(note => note.id !== id)
+    fs.writeFileSync(
+    path.join(__dirname, '../../data/db.json'),
+    JSON.stringify({notes}, null, 2)
+    )
+
+    res.send('Delete was complete')
+  }else{
+    res.status(400).send('The note was not found')
   }
 })
 
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -5,9 +5,6 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'))
 })
 
-
-
-
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'))
 })
